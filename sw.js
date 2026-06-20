c06763e3285c1ba56867f93de2531bdcf4482628
sanjay120405 <luviorshop@gmail.com>
2026-06-20 12:54:48 +0530

    Revert "Add background GPS tracking to tracking.html"
    This reverts commit 94eb17baf55f2aa765259bd901b7ada66567bc90.

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'medwell-track-v1';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', () => {});
